2ab33dcf7b0827e904162fbe85bde39b971cac88
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-16 00:49:50 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { createPool, Pool, QueryResult } from '@vercel/postgres';
+import { createPool, Pool, QueryResult, QueryResultRow } from '@vercel/postgres';
 
 let pool: Pool | null = null;
 
@@ -21,10 +21,13 @@ export async function getPool(): Promise<Pool> {
   return pool;
 }
 
-export async function query<T = any>(text: string, params?: unknown[]): Promise<QueryResult<T>> {
+export async function query<T extends QueryResultRow = QueryResultRow>(
+  text: string, 
+  params?: unknown[]
+): Promise<QueryResult<T>> {
   const pool = await getPool();
   const start = Date.now();
-  const res = await pool.query(text, params);
+  const res = await pool.query<T>(text, params);
   const duration = Date.now() - start;
   console.log('Executed query', { text, duration, rows: res.rowCount });
   return res;
@@ -36,13 +39,11 @@ export async function getClient(): Promise<DatabaseClient> {
   const query = client.query.bind(client);
   const release = client.release.bind(client);
 
-  // Set a timeout of 5 seconds, after which we will log this client's last query
   const timeout = setTimeout(() => {
     console.error('A client has been checked out for more than 5 seconds!');
     console.error(`The last executed query on this client was: ${client.lastQuery}`);
   }, 5000);
 
-  // Monkey patch the query method to keep track of the last query executed
   client.query = (...args: any[]) => {
     client.lastQuery = args;
     return query(...args);
