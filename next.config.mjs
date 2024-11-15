9d63d8314eeb41a494b403359e4722db2d6c9397
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-15 20:27:21 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
