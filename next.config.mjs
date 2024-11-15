268117946a3810071f23c935efd3e3040b3870d8
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-15 21:49:43 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,9 @@
+// next.config.mjs
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
