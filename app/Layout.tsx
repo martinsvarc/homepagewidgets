84462f8d5b435a3c85fba516b1a41a975ee7ab46
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-15 21:53:33 +0100

    Update Layout.tsx

--- app/Layout.tsx
+++ app/Layout.tsx
@@ -1,8 +1,12 @@
+// app/layout.tsx
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import '@/styles/globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ 
+  subsets: ['latin'],
+  weight: ['400', '700'],
+})
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
