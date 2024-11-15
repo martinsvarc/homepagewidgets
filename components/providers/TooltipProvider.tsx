7beff905c1190cb3e5cc0a5eba9426b9e993fc08
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-15 21:01:23 +0100

    Create TooltipProvider.tsx

---
+++ components/providers/TooltipProvider.tsx
@@ -0,0 +1,6 @@
+'use client'
+import { TooltipProvider as RadixTooltipProvider } from "@/components/ui/tooltip"
+
+export function TooltipProvider({ children }: { children: React.ReactNode }) {
+  return <RadixTooltipProvider>{children}</RadixTooltipProvider>
+}
