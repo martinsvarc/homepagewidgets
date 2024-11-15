a8f6c39df4840ea3a9bdaba24c74f8543099eb30
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-16 00:16:21 +0100

    Update progress.tsx

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -3,19 +3,15 @@ import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
 import { cn } from "@/lib/utils"
 
-// Define custom properties interface
-interface CustomCSSProperties extends React.CSSProperties {
-  '--progress-foreground'?: string;
-}
-
-// Update the component props type to include the custom style type
-type ProgressProps = React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & {
-  style?: CustomCSSProperties;
+declare module 'react' {
+  interface CSSProperties {
+    '--progress-foreground'?: string;
+  }
 }
 
 const Progress = React.forwardRef
-  React.ElementRef<typeof ProgressPrimitive.Root>,
-  ProgressProps
+  React.ComponentRef<typeof ProgressPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
@@ -29,7 +25,7 @@ const Progress = React.forwardRef
       className="h-full w-full flex-1 transition-all"
       style={{ 
         transform: `translateX(-${100 - (value || 0)}%)`,
-        backgroundColor: (props.style as CustomCSSProperties)?.['--progress-foreground'] || 'hsl(var(--primary))'
+        backgroundColor: props.style?.['--progress-foreground'] || 'hsl(var(--primary))'
       }}
     />
   </ProgressPrimitive.Root>
