87b16aaf707a212ed795126f6290707e1f5d3661
martinsvarc <77626343+martinsvarc@users.noreply.github.com>
2024-11-16 01:26:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,24 +1,23 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const nextConfig = {
-  // Remove serverActions as it's now enabled by default in Next.js 14
   async rewrites() {
     return {
       fallback: [
         {
           source: '/api/:path*',
           destination: '/api/:path*',
-        },
-      ],
+        }
+      ]
     }
   },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-    };
+      '@': path.resolve(__dirname)
+    }
     return config;
-  },
+  }
 }
 
 module.exports = nextConfig;
